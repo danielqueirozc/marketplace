88c8f18a0b6cb6662334404af7fdeedb5a69d4e4
danielqueirozc <danielqueiroz.job@gmail.com>
2026-09-17 11:15:37 -0300

    feat(ThrottlerModule): ThrottlerGuard implments

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, type MiddlewareConsumer, type NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProxyModule } from './proxy/proxy.module';
 import { ConfigModule } from '@nestjs/config'
 import { ThrottlerModule } from '@nestjs/throttler'
+import { MiddlewareModule } from './middleware/middleware.module';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
+
 
 @Module({
   imports: [
-    ProxyModule,
     ConfigModule.forRoot({
       isGlobal: true
     }),
@@ -16,9 +19,14 @@ import { ThrottlerModule } from '@nestjs/throttler'
         ttl: 60000, // minute
         limit: 100, // 100 requests for minute
       }
-    ])
+    ]),
+    ProxyModule,
+    MiddlewareModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+  ],
 })
 export class AppModule {}
