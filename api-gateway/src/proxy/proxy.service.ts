aab8b57da98959849df62e78a7cfc1c40caa1def
danielqueirozc <danielqueiroz.job@gmail.com>
2026-09-17 10:26:47 -0300

    feat: add proxyRequest and getServiceHealth

--- api-gateway/src/proxy/proxy.service.ts
+++ api-gateway/src/proxy/proxy.service.ts
@@ -1,6 +1,7 @@
-import type { HttpService } from '@nestjs/axios';
+import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { serviceConfig } from '../config/gateway-config';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ProxyService {
@@ -28,9 +29,33 @@ export class ProxyService {
         'x-user-name': userInfo?.name,
         'x-user-role': userInfo?.role,
       }
+
+      const response = await firstValueFrom(
+        this.httpService.request({
+          method: method.toLowerCase(),
+          url,
+          headers: enhancedHeader,
+          timeout: service.timeout,
+        })
+      )
+
+      return response
     } catch (error) {
       this.logger.log(`Error proxing ${method} request to ${serviceName}: ${url}`)
       throw error
+    }
+  }
+
+  async getServiceHealth(serviceName: keyof typeof serviceConfig) {
+    try {
+      const service = serviceConfig[serviceName]
+      const response = await firstValueFrom(
+        this.httpService.get(`${service.url}/health`, { timeout: 3000 })
+      )
+
+      return { status: 'healthy', data: response.data }
+    } catch (error: any) {
+      return { status: 'unhealthy', error: error.message }
     }
   }
 }
